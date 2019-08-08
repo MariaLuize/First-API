942a074ed4d6af56e744bc2586266d58245a316a
Maria Luize <m.luize.s.pinheiro@gmail.com>
2019-08-07 23:43:30 -0300

    Refactor Routes

---
+++ helpers/todos.js
@@ -0,0 +1,53 @@
+const db = require("../models");
+
+exports.getTodos =  function(req, res){ //na verdade a rota é: /api/todos/
+    db.Todo.find() //achar todos os Todos no banco de dados
+    .then(function(todos){// após achar todos, usa-se promise coom .then
+        res.json(todos);
+    })
+    .catch(function(req,res){ // caso haja erro, usa-se .catch para mostrar o erro
+        res.send(err)
+    })
+};
+
+exports.createTodo =  function(req,res){
+    db.Todo.create(req.body)
+    .then(function(newTodo){
+        res.status(201).json(newTodo);
+    })
+    .catche(function(err){
+        res.send(err);
+    })
+};
+
+exports.getTodo =  function(req, res){
+    db.Todo.findById(req.params.todoId)
+    .then(function(foundTodo){
+        res.json(foundTodo);
+    })
+    .catch(function(err){
+        res.send(err);
+    })
+};
+
+exports.updateTodo =  function(req, res){
+    db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})// achar o Id do todo(por meio de req.params.todoId) e trabalhar sobre as informações enviadas pelo req.body
+    .then(function(todo){
+        res.json(todo);
+    })
+    .catch(function(err){
+        res.send(err);
+    })
+};
+
+exports.deleteTodo = function(req, res){
+    db.Todo.remove({_id: req.params.todoId})
+    .then(function(){
+        res.json({message: "Was deleted!"});
+    })
+    .catch(function(err){
+        res.send(err);
+    })
+};
+
+module.exports = exports;
--- routes/todos.js
+++ routes/todos.js
@@ -1,57 +1,22 @@
 const   express     = require("express"),
         router      = express.Router();
 const   db          = require('../models'); //para acessar o banco de dados
+const   helpers     = require('../helpers/todos')
 
 
 
-router.get('/', function(req, res){ //na verdade a rota é: /api/todos/
-    db.Todo.find() //achar todos os Todos no banco de dados
-    .then(function(todos){// após achar todos, usa-se promise coom .then
-        res.json(todos);
-    })
-    .catch(function(req,res){ // caso haja erro, usa-se .catch para mostrar o erro
-        res.send(err)
-    })
-});
+router.route('/')              //Equivale à:
+    .get(helpers.getTodos)     // router.get('/',);
+    .put(helpers.createTodo)   // router.post('/',);
 
-router.post('/', function(req,res){
-    db.Todo.create(req.body)
-    .then(function(newTodo){
-        res.status(201).json(newTodo);
-    })
-    .catche(function(err){
-        res.send(err);
-    })
-})
 
-router.get('/:todoId', function(req, res){
-    db.Todo.findById(req.params.todoId)
-    .then(function(foundTodo){
-        res.json(foundTodo);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-})
-
-router.put('/:todoId', function(req, res){
-    db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})// achar o Id do todo(por meio de req.params.todoId) e trabalhar sobre as informações enviadas pelo req.body
-    .then(function(todo){
-        res.json(todo);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-})
-
-router.delete('/:todoId', function(req, res){
-    db.Todo.remove({_id: req.params.todoId})
-    .then(function(){
-        res.json({message: "Was deleted!"});
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-})
+// router.get('/:todoId',)
+// router.put('/:todoId',)
+// router.delete('/:todoId',)
+//refatorando:
+router.route('/:todoId')
+    .get(helpers.getTodo)
+    .put(helpers.updateTodo)
+    .delete(helpers.deleteTodo)
 
 module.exports = router;
