9914b84cb005b90c3eae1c981f4db414b7bde0ed
Maria Luize <m.luize.s.pinheiro@gmail.com>
2019-08-07 23:18:03 -0300

    Basic routes

--- routes/todos.js
+++ routes/todos.js
@@ -24,4 +24,34 @@ router.post('/', function(req,res){
     })
 })
 
+router.get('/:todoId', function(req, res){
+    db.Todo.findById(req.params.todoId)
+    .then(function(foundTodo){
+        res.json(foundTodo);
+    })
+    .catch(function(err){
+        res.send(err);
+    })
+})
+
+router.put('/:todoId', function(req, res){
+    db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})// achar o Id do todo(por meio de req.params.todoId) e trabalhar sobre as informações enviadas pelo req.body
+    .then(function(todo){
+        res.json(todo);
+    })
+    .catch(function(err){
+        res.send(err);
+    })
+})
+
+router.delete('/:todoId', function(req, res){
+    db.Todo.remove({_id: req.params.todoId})
+    .then(function(){
+        res.json({message: "Was deleted!"});
+    })
+    .catch(function(err){
+        res.send(err);
+    })
+})
+
 module.exports = router;
